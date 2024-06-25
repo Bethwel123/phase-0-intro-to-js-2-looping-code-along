e32bc48ce5fb32a072c71fc3407b040817aa3ee1
Bethwel123 <bethwellangat654@gmail.com>
2024-06-26 02:24:59 +0300

    Adds loops to index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+   return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+ }
+
+function countDown(count) {
+   for (let i = count; i >= 0; i--) {
+     console.log(i);
+   }
+ }
+ 
+
